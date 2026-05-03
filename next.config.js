124e794f40ff90daed9a3c446607210498a92a91
Kien Nguyen <kiennguyen2701@gmail.com>
2026-05-03 23:40:32 +0700

    fix config vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -18,8 +17,8 @@ const nextConfig: NextConfig = {
   },
 
   typescript: {
-    ignoreBuildErrors: true, // 🔥 fix build Vercel
+    ignoreBuildErrors: true, // 🔥 FIX BUILD
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
